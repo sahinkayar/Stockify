65aed35c4ae49b4bfa328b57f1105dc449a0362f
sahinkayar <sahinkayar09@gmail.com>
2026-09-20 10:24:22 +0300

    migration update

--- frontend/src/Services/AuthService.tsx
+++ frontend/src/Services/AuthService.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { handleError } from "../Helpers/ErrorHandler";
 import type { UserProfileToken } from "../Models/User";
 
-const api = "https://stockify-1-7f09.onrender.com/api";
+const api = "https://stockify-exms.onrender.com/api";
 
 export const LoginAPI = async (username: string, password: string) => {
   try {
